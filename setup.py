4736fb1ad3c0603336c4f77163e536833bebfdfe
Thibaut Lamadon <thibaut.lamadon@gmail.com>
2014-04-26 00:05:08 +0200

    creating new version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tensorparser',
-      version='0.11',
+      version='0.12',
       description='Generate code from tensor expressions',
       url='https://github.com/tlamadon/tensorparser',
       author='Thibaut Lamadon',
